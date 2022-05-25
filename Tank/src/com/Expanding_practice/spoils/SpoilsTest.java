e0e81b42d503dfbb52fbe45987da63d7dedd228c
root <sakai263016@gmail.com>
2022-05-25 14:38:42 +0800

    version_0.0.1

--- Tank/src/com/Expanding_practice/spoils/SpoilsTest.java
+++ Tank/src/com/Expanding_practice/spoils/SpoilsTest.java
@@ -2,7 +2,7 @@ package com.Expanding_practice.spoils;
 
 import javax.swing.*;
 
-public class Test {
+public class SpoilsTest {
     public static void main(String[] args) {
         JFrame frame = new JFrame("TANK");
         SpoilsPanel spoilsPanel = new SpoilsPanel();
