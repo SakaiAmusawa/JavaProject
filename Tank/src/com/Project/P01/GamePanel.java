364e27090c7faef01870d636b802798252d10a93
root <sakai263016@gmail.com>
2022-05-25 19:56:42 +0800

    version_0.0.1

--- Tank/src/com/Project/P01/GamePanel.java
+++ Tank/src/com/Project/P01/GamePanel.java
@@ -6,14 +6,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements KeyListener {
-    Tank tank = new Tank(100, 100);
+    Tank tank = new Tank(450, 900);
 
     public void paint(Graphics g) {
         super.paint(g);
         this.setBackground(Color.BLACK);
         ImageIcon icon = new ImageIcon(tank.getUrl());
         Image image = icon.getImage();
-        g.drawImage(image, tank.getX(), tank.getY(), 20, 20, this);
+        g.drawImage(image, tank.getX(), tank.getY(), 50, 50, this);
     }
 
 
