5281726793fc582eab9ecef3e92c4a4a497f5b2d
root <sakai263016@gmail.com>
2022-05-25 11:36:52 +0800

    version_0.0.1

---
+++ Tank/src/com/Expanding_practice/spoils/Spoils.java
@@ -0,0 +1,34 @@
+package com.Expanding_practice.spoils;
+
+import java.util.Random;
+
+public class Spoils {
+    Random random = new Random();
+    private int x = random.nextInt(600);
+    private int y = random.nextInt(600);
+    private String Url = "img/spoils.jpg";
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+}
--- Tank/src/com/Expanding_practice/spoils/SpoilsPanel.java
+++ Tank/src/com/Expanding_practice/spoils/SpoilsPanel.java
@@ -2,13 +2,12 @@ package com.Expanding_practice.spoils;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 public class SpoilsPanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
-        Random random = new Random();
-        ImageIcon icon = new ImageIcon("img/spoils.jpg");
-        g.drawImage(icon.getImage(), random.nextInt(600), random.nextInt(600), this);
+        Spoils spoils = new Spoils();
+        ImageIcon icon = new ImageIcon(spoils.getUrl());
+        g.drawImage(icon.getImage(), spoils.getX(), spoils.getY(), this);
     }
 }
