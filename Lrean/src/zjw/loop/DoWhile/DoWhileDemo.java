c7155f1a822e63f441755c23be98b2af1175537c
root <sakai263016@gmail.com>
2022-05-26 21:55:11 +0800

    learn

---
+++ Lrean/src/zjw/loop/DoWhile/DoWhileDemo.java
@@ -0,0 +1,11 @@
+package zjw.loop.DoWhile;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("hello");
+            i++;
+        } while (i < 3);
+    }
+}
