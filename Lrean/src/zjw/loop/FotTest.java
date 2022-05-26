c9bb67a498402981c529af21382687e31583a583
root <sakai263016@gmail.com>
2022-05-26 21:37:17 +0800

    learn

---
+++ Lrean/src/zjw/loop/FotTest.java
@@ -0,0 +1,15 @@
+package zjw.loop;
+//水仙花数案例
+public class FotTest {
+    public static void main(String[] args) {
+        for (int i = 100; i < 999; i++) {
+            int g = i % 10;
+            int s = i / 10 % 10;
+            int b = i / 100;
+            if (g * g * g + s * s * s + b * b * b == i) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
