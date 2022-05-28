96b64a167a386d5b0545e409c345f45b2eab0a83
root <sakai263016@gmail.com>
2022-05-28 21:48:21 +0800

    learn

---
+++ Learn/src/zjw/loop/For/ForTest_01.java
@@ -0,0 +1,11 @@
+package zjw.loop.For;
+
+public class ForTest_01 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 5; i++) {
+            sum += i;
+        }
+        System.out.println("sum(1-5):" + sum);
+    }
+}
---
+++ Learn/src/zjw/loop/RandomDemo_exp.java
@@ -0,0 +1,53 @@
+package zjw.loop;
+
+import java.util.Random;
+import java.util.Scanner;
+//该实例代码来源于：https://blog.csdn.net/Tony_Starkis/article/details/119147428
+public class RandomDemo_exp {
+    public static void main(String[] args) {
+        guessNumberGame();
+    }
+
+    public static int menu() {
+        System.out.println("***********************");
+        System.out.println("  1、play      0、exit  ");
+        System.out.println("***********************");
+        System.out.println("请输入您的选择：");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void game() {
+        Random random = new Random();
+        int toGuess = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("请输入你猜测的数：");
+            int num = scanner.nextInt();
+            if (num > toGuess) {
+                System.out.println("猜大了");
+            } else if (num < toGuess) {
+                System.out.println("猜小了");
+            } else {
+                System.out.println("恭喜你，猜对了！");
+                break;
+            }
+        }
+    }
+
+    public static void guessNumberGame() {
+        while (true) {
+            int choice = menu();
+            if (choice == 1) {
+                game();
+            } else if (choice == 0) {
+                System.out.println("白白~");
+                break;
+            } else {
+                System.out.println("输入错误，请重试...");
+            }
+        }
+    }
+
+
+}
