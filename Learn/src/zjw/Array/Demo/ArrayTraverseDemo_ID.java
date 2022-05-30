9d0de4b310c0c9ac406b456974c1810b9a5932a3
root <sakai263016@gmail.com>
2022-05-30 18:04:06 +0800

    learn

--- Learn/src/zjw/Array/Demo/ArrayTraverseDemo_ID.java
+++ Learn/src/zjw/Array/Demo/ArrayTraverseDemo_ID.java
@@ -23,9 +23,9 @@ public class ArrayTraverseDemo_ID {
             //随机一个索引位置出来 Id[index]
             int index = random.nextInt(Id.length);
 
-            int temp = Id[index];//定义一个临时变量储存index位置处的值
+            int temporary = Id[index];//定义一个临时变量储存index位置处的值
             Id[index] = Id[i];   //让index索引处的值等于i处的值
-            Id[i] = temp;        //再让i处的值等于原来index的值保留数据的完整性，如果不替换就会出现重复的数值
+            Id[i] = temporary;        //再让i处的值等于原来index的值保留数据的完整性，如果不替换就会出现重复的数值
         }
         for (int i : Id) {
             System.out.print(i + "\t");
