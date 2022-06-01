7bd8204667b143352ad86aaa7beefcd1206e643e
root <sakai263016@gmail.com>
2022-06-01 15:03:46 +0800

    learn

---
+++ Learn/src/zjw/Method/MethodDemo_02.java
@@ -0,0 +1,17 @@
+package zjw.Method;
+
+public class MethodDemo_02 {
+    //学习方法的完整定义格式，并理解其调用和执行流程
+    public static void main(String[] args) {
+        print();
+    }
+
+    /*
+     * 无参数 无返回值 类型
+     */
+    public static void print() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("hello world");
+        }
+    }
+}
