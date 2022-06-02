5eab585b804216fd0ee63ac1a8fdebad7e76b19c
root <sakai263016@gmail.com>
2022-06-02 08:31:14 +0800

    learn

--- Learn/src/zjw/Method/param/MethodParam01.java
+++ Learn/src/zjw/Method/param/MethodParam01.java
@@ -1,14 +1,16 @@
 package zjw.Method.param;
+
 //Öµ´«µÝ
 public class MethodParam01 {
     public static void main(String[] args) {
-        int a =10;
+        int a = 10;
         change(a);
         System.out.println(a);//10
     }
-    public static void change(int a ){
+
+    public static void change(int a) {
         System.out.println(a);//10
-        a=20;
+        a = 20;
         System.out.println(a);//20
     }
 }
