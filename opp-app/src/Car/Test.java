c1f81783c7502c9e9ad8ac0d4dbbda62019b1e2b
root <sakai263016@gmail.com>
2022-06-08 11:20:41 +0800

    learn

--- opp-app/src/Car/Test.java
+++ opp-app/src/Car/Test.java
@@ -4,27 +4,27 @@ public class Test {
     public static void main(String[] args) {
 
 
-        Car car = new Car();
+        Car car1 = new Car();
 
-        car.name = "X5";
-        car.price = 50.0;
+        car1.name = "X5";
+        car1.price = 50.0;
 
-        System.out.println(car.name);
-        System.out.println(car.price);
+        System.out.println(car1.name);
+        System.out.println(car1.price);
 
-        car.Start();
-        car.Price();
+        car1.Start();
+        car1.Price();
 
         System.out.println("-------------");
 
-        Car car1 = new Car();
+        Car car2 = new Car();
 
-        car1.name = "gtr";
-        car1.price = 69.5;
+        car2.name = "gtr";
+        car2.price = 69.5;
 
-        System.out.println(car1.name);
+        System.out.println(car2.name);
 
-        car1.Price();
+        car2.Price();
 
     }
 }
